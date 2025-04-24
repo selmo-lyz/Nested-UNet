e99a0ca57a80cb9183dde72c79e9a88da74117de
selmo-lyz <yzlin1999@gmail.com>
2025-04-24 23:53:21 +0800

    refactor: Merge _calc_metric() into _run_epoch()

--- src/trainer.py
+++ src/trainer.py
@@ -35,43 +35,10 @@ class NestedUNetTrainer:
             return metric.__class__.__name__
         return str(metric)
 
-    def _calc_metrics(
-        self,
-        dataloader,
-        metric_fns,
-    ):
-        if len(metric_fns) <= 0:
-            print("[ERROR] There are no metric functions in the parameter metric_fns")
-            return
-
-        self.model.eval()
-        running_metrics = [0 for i in range(len(metric_fns))]
-        total_samples = 0
-
-        for batch in tqdm(dataloader, ncols=80):
-            inputs = batch["image"].to(self.device)
-            labels = batch["mask"].to(self.device)
-            batch_size = inputs.size(0)
-
-            with torch.no_grad():
-                outputs = self.model(inputs)
-                outputs = torch.stack(outputs, dim=0)
-                preds = torch.mean(outputs, dim=0)
-
-                for idx, metric_fn in enumerate(metric_fns):
-                    metric = metric_fn(preds, labels)
-                    running_metrics[idx] += metric * batch_size
-                total_samples += batch_size
-
-        avg_metrics = {
-            self._get_metric_name(metric_fn): float(running_metric) / total_samples
-            for (metric_fn, running_metric) in zip(metric_fns, running_metrics)
-        }
-        return avg_metrics
-
-    def _run_epochs(self, dataloader, is_train=True):
+    def _run_epochs(self, dataloader, is_train=True, metric_fns=[]):
         self.model.train() if is_train else self.model.eval()
         running_loss = 0.0
+        running_metrics = [0 for i in range(len(metric_fns))]
         total_samples = 0
 
         for batch in tqdm(dataloader, ncols=80):
@@ -82,18 +49,27 @@ class NestedUNetTrainer:
             with torch.set_grad_enabled(is_train):
                 outputs = self.model(inputs)
                 outputs = torch.stack(outputs, dim=0)
-                outputs = torch.mean(outputs, dim=0)
-                loss = self.loss_fn(outputs, labels)
+                preds = torch.mean(outputs, dim=0)
 
                 if is_train:
+                    loss = self.loss_fn(preds, labels)
                     self.optimizer.zero_grad()
                     loss.backward()
                     self.optimizer.step()
+                    running_loss += loss.item() * batch_size
+                else:
+                    for idx, metric_fn in enumerate(metric_fns):
+                        metric = metric_fn(preds, labels)
+                        running_metrics[idx] += metric * batch_size
 
-            running_loss += loss.item() * batch_size
             total_samples += batch_size
 
-        return running_loss / total_samples
+        avg_loss = running_loss / total_samples
+        avg_metrics = {
+            self._get_metric_name(metric_fn): float(running_metric) / total_samples
+            for (metric_fn, running_metric) in zip(metric_fns, running_metrics)
+        }
+        return avg_loss if len(metric_fns) == 0 else avg_metrics
 
     def fit(
         self,
@@ -114,7 +90,11 @@ class NestedUNetTrainer:
                 cb.on_epoch_begin(self, epoch)
 
             train_loss = self._run_epochs(train_dataloader, is_train=True)
-            val_metrics = self._calc_metrics(val_dataloader, metric_fns)
+            val_metrics = self._run_epochs(
+                val_dataloader,
+                is_train=False,
+                metric_fns=metric_fns,
+            )
 
             logs = {
                 "train_loss": train_loss,
